e20a46e61edc977ae374446e3e5070a1d0dc190a
Andrew Snyder <andrew.snyder9702@gmail.com>
2025-02-18 17:02:50 -0500

    Finishes enumerables exercises

--- lessons/enumerables/exercises/find.rb
+++ lessons/enumerables/exercises/find.rb
@@ -1,14 +1,29 @@
 # 1. Using #each, write the code to find the first sister over four letters in length.
 # 2. Once you get the method below to work using #each, refactor your code to use #find. 
 
+# def find_first_long_named_sister
+#     sisters = ["Rose", "Kathleen", "Eunice", "Patricia", "Jean"]
+  
+#     ### YOUR CODE HERE
+
+#     first_long_named_sister = []
+
+#     sisters.each do |sister|
+#         if sister.length > 4
+#             first_long_named_sister << sister
+#         end
+#     end
+
+#     first_long_named_sister.first
+#     # should return "Kathleen"
+# end
+
+
 def find_first_long_named_sister
     sisters = ["Rose", "Kathleen", "Eunice", "Patricia", "Jean"]
-  
-    ### YOUR CODE HERE
 
-    # should return "Kathleen"
+    sisters.find do |sister|
+        sister.length > 4
+    end
 end
-
-
-
 p find_first_long_named_sister
--- lessons/enumerables/exercises/find_all.rb
+++ lessons/enumerables/exercises/find_all.rb
@@ -2,13 +2,31 @@
 # 2. Once you get the method below to work using #each, refactor your code to use #find_all. 
 
 
+# def find_all_nums_divisible_by_3
+#     numbers = [1,2,3,4,5,6,7,8,9]
+    
+#     ### YOUR CODE HERE
+#     numbers_divisible_by_3 = []
+
+#     numbers.each do |number|
+#         numbers_divisible_by_3 << number if number % 3 == 0
+#     end
+
+#     numbers_divisible_by_3
+#     # should return [3,6,9]
+# end
+
+
+# p find_all_nums_divisible_by_3
+
 def find_all_nums_divisible_by_3
     numbers = [1,2,3,4,5,6,7,8,9]
-    
-    ### YOUR CODE HERE
-  
-    # should return [3,6,9]
-end
 
+    numbers_by_3 = numbers.find_all do |num|
+        num % 3 == 0
+    end
+
+    numbers_by_3
+end
 
 p find_all_nums_divisible_by_3
